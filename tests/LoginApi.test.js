aaecdca777dd8d04113025d0c3bc91d3fc0368c3
sandworm47 <owen.cleland@gmail.com>
2023-01-21 17:48:14 -0700

    Login Api tests

---
+++ tests/LoginApi.test.js
@@ -0,0 +1,20 @@
+import fetch from 'node-fetch';
+
+it ("Should get a login token", async () => { //async allows us to read from the database by moving on and coming back
+
+    const loginResponse = await fetch('https://dev.stedi.me/login', { //await fetch allows it to move on while getting the data
+
+        method: 'POST', //method : POST says to give information to the server
+        headers:{
+            'Content-Type': 'application/text'
+        },
+
+        body:JSON.stringify({ //makes the following into JSON text to be read by the server
+            "userName": "lame4198@gmail.com",
+            "password": "Hellothere66!"
+        })
+
+    })
+    expect(loginResponse.status).toBe(200); //200 means authorized
+
+})
